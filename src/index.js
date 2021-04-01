b58a9a2361f8e9229659e622711a697c79cc1058
takutaku1256 <ryoll329555@docomo.ne.jp>
2021-04-01 19:43:09 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('sw.js')
+    .then((registration) => {
+      console.log('登録成功: ', registration.scope);
+    })
+    .catch((err) => {
+      console.log('登録失敗: ', err);
+    });
+}
